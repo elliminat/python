276d9d1871587c0253fda0619f1ac21873fe1d36
Ilya Gorbunov <36045588+elliminat@users.noreply.github.com>
2020-11-08 16:37:52 +0700

    reload

--- test_yandex.py
+++ test_yandex.py
@@ -1,11 +1,8 @@
 import pytest
 import page
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
+
 
 
 options = webdriver.ChromeOptions()
